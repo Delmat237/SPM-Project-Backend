b258c32510b4d0d3eb1e5e80250a6c8ac1665c90
Delmat237 <azangueleonel9@gmail.com>
2026-05-23 08:22:32 +0100

    fix : securitéy

--- src/main/java/com/techwave/auth/user/security/SecurityConfig.java
+++ src/main/java/com/techwave/auth/user/security/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList(
-
+               "http://localhost:3000",
                "http://localhost:5173"              // Optionnel, utile si tu veux encore tester en local
         ));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
